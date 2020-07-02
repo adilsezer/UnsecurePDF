5c8a1058bab79143c4fd3a17090b228c0d940a1f
Adil Sezer <sezeradil@gmail.com>
2020-07-02 21:01:47 +0300

    Add files via upload

---
+++ UnsecurePDF.py
@@ -0,0 +1,17 @@
+import pikepdf
+import os
+from tkinter import filedialog
+from tkinter import *
+
+root = Tk()
+root.withdraw()
+dirpath = filedialog.askdirectory()
+
+for filename in os.listdir(dirpath):
+    if filename.lower().endswith(".pdf"):
+        os.rename(dirpath + "/" + filename, dirpath + "/Old-" + filename)
+        with pikepdf.open(dirpath + "/Old-" + filename) as pdf:
+            pdf.save(dirpath + "/" + filename)
+        os.remove(dirpath + "/Old-" + filename)
+
+print("Done!")
